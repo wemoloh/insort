2f0b07ad75da8ffbb2b026da2e200139a1565c80
wemoloh <bart@bartrobinson.com>
2017-01-08 22:08:22 -0700

    added TidySet and TidyObject

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
-let TidyMap = require('..');
+let {TidyMap, TidySet, TidyObject} = require('..');
 let assert = require('assert');
 
 const SORTED = [['b', 42], ['q', -8], ['x', 23]];
 const UNSORTED = [['b', 42], ['x', 23], ['q', -8]];
+
+const SORTED_KEYS = ['b', 'q', 'x'];
+const UNSORTED_KEYS = ['b', 'x', 'q'];
+
+const SORTED_OBJECT = {b: 42, q: -8, x: 23};
+const UNSORTED_OBJECT = {b: 42, x: 23, q: -8};
 
 describe('TidyMap', function() {
   describe('#constructor()', function() {
@@ -29,7 +35,7 @@ describe('TidyMap', function() {
 
     it('cmp custom', function() {
       let m = new TidyMap([[0, 42], [10, -8], [2, 23]],
-                             (a, b) => a - b);
+                          (a, b) => a - b);
       assert.deepStrictEqual([...m], [[0, 42], [2, 23], [10, -8]]);
     });
 
@@ -39,7 +45,7 @@ describe('TidyMap', function() {
     });
   });
 
-  describe('#set', function() {
+  describe('#set()', function() {
     it('new at beginning', function() {
       let m = new TidyMap(UNSORTED);
       assert.strictEqual(m, m.set('a', 99));
@@ -149,6 +155,236 @@ describe('TidyMap', function() {
       let m = new TidyMap(UNSORTED);
       assert(m.has(SORTED[0][0]));
       assert(!m.has('g'));
+    });
+  });
+});
+
+describe('TidySet', function() {
+  describe('#constructor()', function() {
+    it('no args', function() {
+      let s = new TidySet();
+      assert.deepStrictEqual([...s], []);
+    });
+
+    it('sorted', function() {
+      let s = new TidySet(SORTED_KEYS);
+      assert.deepStrictEqual([...s], SORTED_KEYS);
+    });
+
+    it('unsorted', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.deepStrictEqual([...s], SORTED_KEYS);
+    });
+
+    it('cmp default', function() {
+      // default compare is lexicographic, not numerical
+      let s = new TidySet([0, 10, 2]);
+      assert.deepStrictEqual([...s], [0, 10, 2]);
+    });
+
+    it('cmp custom', function() {
+      let s = new TidySet([0, 10, 2],
+                          (a, b) => a - b);
+      assert.deepStrictEqual([...s], [0, 2, 10]);
+    });
+
+    it('inheritance', function() {
+      let s = new TidySet();
+      assert(s instanceof Set);
+    });
+  });
+
+  describe('#add()', function() {
+    it('at beginning', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.strictEqual(s, s.add('a'));
+      assert.deepStrictEqual([...s], ['a'].concat(SORTED_KEYS));
+    });
+
+    it('at end', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.strictEqual(s, s.add('z'));
+      assert.deepStrictEqual([...s], SORTED_KEYS.concat(['z']));
+    });
+
+    it('in middle', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.strictEqual(s, s.add('g'));
+      assert.deepStrictEqual([...s], ['b', 'g', 'q', 'x']);
+    });
+
+    it('existing', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.strictEqual(s, s.add('q'));
+      assert.deepStrictEqual([...s], SORTED_KEYS);
+    });
+  });
+
+  describe('#delete()', function() {
+    it('existing', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert(s.delete(SORTED_KEYS[1]));
+      assert.deepStrictEqual([...s], [SORTED_KEYS[0], SORTED_KEYS[2]]);
+    });
+
+    it('missing', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert(!s.delete('g'));
+      assert.deepStrictEqual([...s], SORTED_KEYS);
+    });
+  });
+
+  describe('other overridden methods', function() {
+    it('#clear()', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.strictEqual(void 0, s.clear());
+      assert.deepStrictEqual([...s], []);
+    });
+
+    it('#entries()', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      let it = s.entries();
+      assert.strictEqual(it[Symbol.iterator](), it);
+      for (let key of SORTED_KEYS) {
+        assert.deepStrictEqual(it.next(), {value: [key, key], done: false});
+      }
+      assert.deepStrictEqual(it.next(), {value: void 0, done: true});
+    });
+
+    it('#forEach()', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      let i = 0;
+      s.forEach((v, k, that) => {
+        let key = SORTED_KEYS[i++];
+        assert.deepStrictEqual([k, v], [key, key]);
+        assert.strictEqual(that, s);
+      });
+    });
+
+    it('#keys()', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      let it = s.keys();
+      assert.strictEqual(it[Symbol.iterator](), it);
+      for (let key of SORTED_KEYS) {
+        assert.deepStrictEqual(it.next(), {value: key, done: false});
+      }
+      assert.deepStrictEqual(it.next(), {value: void 0, done: true});
+    });
+
+    it('#values()', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      let it = s.values();
+      assert.strictEqual(it[Symbol.iterator](), it);
+      for (let key of SORTED_KEYS) {
+        assert.deepStrictEqual(it.next(), {value: key, done: false});
+      }
+      assert.deepStrictEqual(it.next(), {value: void 0, done: true});
+    });
+  });
+
+  describe('inherited properties', function() {
+    it('#size', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert.strictEqual(SORTED_KEYS.length, s.size);
+      s.add('g');
+      assert.strictEqual(SORTED_KEYS.length + 1, s.size);
+      s.delete('g');
+      assert.strictEqual(SORTED_KEYS.length, s.size);
+    });
+
+    it('#has()', function() {
+      let s = new TidySet(UNSORTED_KEYS);
+      assert(s.has(SORTED_KEYS[0]));
+      assert(!s.has('g'));
+    });
+  });
+});
+
+describe('TidyObject', function() {
+  describe('#constructor()', function() {
+    it('no args', function() {
+      let o = new TidyObject();
+      assert.deepEqual(o, {});
+    });
+
+    it('sorted', function() {
+      let o = new TidyObject(SORTED_OBJECT);
+      assert.deepEqual(o, SORTED_OBJECT);
+      assert.deepStrictEqual(Object.keys(o),
+                             Object.keys(SORTED_OBJECT).sort());
+    });
+
+    it('unsorted', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      assert.deepEqual(o, SORTED_OBJECT);
+      assert.deepStrictEqual(Object.keys(o),
+                             Object.keys(SORTED_OBJECT).sort());
+    });
+
+    it('cmp default', function() {
+      // default compare is lexicographic, not numerical
+      let o = new TidyObject({'0': 42, '10': -8, '2': 23});
+      assert.deepStrictEqual(Object.keys(o), ['0', '10', '2']);
+    });
+
+    it('cmp custom', function() {
+      let o = new TidyObject({'0': 42, '10': -8, '2': 23},
+                             (a, b) => parseInt(a, 10) - parseInt(b, 10));
+      assert.deepStrictEqual(Object.keys(o), ['0', '2', '10']);
+    });
+  });
+
+  describe('set', function() {
+    it('new at beginning', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      o.a = 99;
+      let expected = Object.assign({}, SORTED_OBJECT, {a: 99});
+      assert.deepEqual(o, expected);
+      assert.deepStrictEqual(Object.keys(o), Object.keys(expected).sort());
+    });
+
+    it('new at end', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      o.z = 0;
+      let expected = Object.assign({}, SORTED_OBJECT, {z: 0});
+      assert.deepEqual(o, expected);
+      assert.deepStrictEqual(Object.keys(o), Object.keys(expected).sort());
+    });
+
+    it('new in middle', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      o.g = 13;
+      let expected = Object.assign({}, SORTED_OBJECT, {g: 13});
+      assert.deepEqual(o, expected);
+      assert.deepStrictEqual(Object.keys(o), Object.keys(expected).sort());
+    });
+
+    it('existing', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      o.q = 99;
+      let expected = Object.assign({}, SORTED_OBJECT, {q: 99});
+      assert.deepEqual(o, expected);
+      assert.deepStrictEqual(Object.keys(o), Object.keys(expected).sort());
+    });
+  });
+
+  describe('delete', function() {
+    it('existing', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      delete o.q;
+      assert.strictEqual(void 0, o.q);
+      let expected = {b: 42, x: 23};
+      assert.deepEqual(o, expected);
+      assert.deepStrictEqual(Object.keys(o), Object.keys(expected).sort());
+    });
+
+    it('missing', function() {
+      let o = new TidyObject(UNSORTED_OBJECT);
+      delete o.g;
+      assert.strictEqual(void 0, o.g);
+      assert.deepEqual(o, SORTED_OBJECT);
+      assert.deepStrictEqual(Object.keys(o),
+                             Object.keys(SORTED_OBJECT).sort());
     });
   });
 });
--- tidymap.js
+++ tidymap.js
@@ -1,13 +1,13 @@
 /**
- * tidymap module: provides TidyMap class.
+ * tidymap module: provides TidyMap, TidySet, and TidyObject.
  */
 
 (function() {
   'use strict';
 
   // Module constants
-  const CMP = Symbol('compare function');
-  const ORDER = Symbol('order array');
+  const CMP = Symbol('tidymap.CMP');
+  const ORDER = Symbol('tidymap.ORDER');
   const CMP_DEFAULT = (a, b) => String(a).localeCompare(b);
 
   // Binary search
@@ -36,7 +36,7 @@
      *
      * @param {Iterable} [entries] - Key-value pairs to initialize map.  They
      * do not have to be pre-sorted.
-     * @param {Function} [cmp] - Function to compare two elements, just as in
+     * @param {Function} [cmp] - Function to compare two keys, just as in
      * Array#sort().  Default uses String#localeCompare().
      */
     constructor(entries = [], cmp = CMP_DEFAULT) {
@@ -103,6 +103,127 @@
   }
 
   TidyMap.prototype[Symbol.iterator] = TidyMap.prototype.entries;
+
+  TidyMap.TidyMap = TidyMap;
+
+  /**
+   * Set subclass that efficiently maintains a sorted iteration order.
+   */
+  class TidySet extends Set {
+
+    /**
+     * Create a new TidySet.
+     *
+     * @param {Iterable} [entries] - Elements to initialize set.  They do not
+     * have to be pre-sorted.
+     * @param {Function} [cmp] - Function to compare two elements, just as in
+     * Array#sort().  Default uses String#localeCompare().
+     */
+    constructor(entries = [], cmp = CMP_DEFAULT) {
+      super();
+
+      let order = [];
+      for (let key of entries) {
+        let oldSize = this.size;
+        super.add(key);
+        if (oldSize !== this.size) {
+          order.push(key);
+        }
+      }
+
+      this[ORDER] = order.sort(cmp);
+      this[CMP] = cmp;
+    }
+
+    add(key) {
+      let oldSize = this.size;
+      super.add(key);
+      if (oldSize !== this.size) {
+        let order = this[ORDER];
+        order.splice(bisect(order, this[CMP], key), 0, key);
+      }
+      return this;
+    }
+
+    clear() {
+      super.clear();
+      this[ORDER].length = 0;
+    }
+
+    delete(key) {
+      let had = super.delete(key);
+      if (had) {
+        let order = this[ORDER];
+        order.splice(bisect(order, this[CMP], key), 1);
+      }
+      return had;
+    }
+
+    * entries() {
+      for (let key of this[ORDER]) {
+        yield [key, key];
+      }
+    }
+
+    forEach(f, that) {
+      for (let key of this[ORDER]) {
+        f.call(that, key, key, this);
+      }
+    }
+
+    values() {
+      return this[ORDER][Symbol.iterator]();
+    }
+  }
+
+  TidySet.prototype.keys = TidySet.prototype[Symbol.iterator] =
+    TidySet.prototype.values;
+
+  TidyMap.TidySet = TidySet;
+
+  /**
+   * Object subclass that efficiently maintains a sorted iteration order.
+   */
+  class TidyObject {
+
+    /**
+     * Create a new TidyObject.
+     *
+     * @param {*} [src] - Source object whose own enumerable properties will be
+     * shallow copied.
+     * @param {Function} [cmp] - Function to compare two keys, just as in
+     * Array#sort().  Default uses String#localeCompare().
+     */
+    constructor(src = {}, cmp = CMP_DEFAULT) {
+      Object.assign(this, src);
+
+      let order = Object.keys(src).sort(cmp);
+
+      return new Proxy(this, {
+        ownKeys: () => order,
+
+        set: (that, key, val) => {
+          let had = Object.prototype.hasOwnProperty.call(that, key);
+          that[key] = val;
+          if (!had) {
+            order.splice(bisect(order, cmp, key), 0, key);
+          }
+          return true;
+        },
+
+        deleteProperty: (that, key) => {
+          let had = Object.prototype.hasOwnProperty.call(that, key);
+          if (had) {
+            delete that[key];
+            order.splice(bisect(order, cmp, key), 1);
+          }
+          return true;
+        }
+      });
+    }
+  }
+
+  TidyMap.TidyObject = TidyObject;
 
   // export
   if (typeof module !== 'undefined' && !module.nodeType && module.exports) {
